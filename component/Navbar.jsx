d5f7348cf57b03c38039e7019546cc0bf35ee99d
gitKrakenite <johnspaxxvile@gmail.com>
2022-01-18 15:12:10 +0300

    Second commit
    Added styling and reponsivity to the app

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -2,57 +2,56 @@ import Image from 'next/image';
 import React from 'react';
 import styles from "../styles/Navbar.module.css";
 
-const Navbar = () => {
-    return (
-        <div className={styles.container}>
+const Navbar = () => (
+    <div className={styles.container}>
 
-            <div className={styles.item}>
+        <div className={styles.item}>
 
-                <div className={styles.callButton}>
-                    <Image src="/Images/telephone.png" alt="" width="32" height="32" />
-                </div>
-
-                <div className={styles.texts}>
-
-                    <div className={styles.text}>ORDER NOW!</div>
-
-                    <div className={styles.text}>012 345 678</div>
-
-                </div>
-
+            <div className={styles.callButton}>
+                <Image src="/Images/telephone.png" alt="" width="32" height="32" />
             </div>
 
-            {/* Middle stuff */}
-            <div className={styles.item}>
+            <div className={styles.texts}>
 
-                <ul className={styles.list}>
+                <div className={styles.text}>ORDER NOW!</div>
 
-                    <li className={styles.listItem}>Homepage</li>
+                <div className={styles.text}>012 345 678</div>
 
-                    <li className={styles.listItem}>Products</li>
-
-                    <li className={styles.listItem}>Menu</li>
-
-                    <Image src="/Images/logo.png" alt="" width="160px" height="69px" />
-
-                    <li className={styles.listItem}>Events</li>
-
-                    <li className={styles.listItem}>Blog</li>
-
-                    <li className={styles.listItem}>Contact</li>
-                </ul>
-
-            </div>
-
-            <div className={styles.item}>
-                <div className={styles.cart}>
-                    <Image src="/Images/cart.png" alt="" width="30px" height="30px" />
-                    <div className={styles.counter}>2</div>
-                </div>
             </div>
 
         </div>
-    )
-}
+
+        {/* Middle stuff */}
+        <div className={styles.item}>
+
+            <ul className={styles.list}>
+
+                <li className={styles.listItem}>Homepage</li>
+
+                <li className={styles.listItem}>Products</li>
+
+                <li className={styles.listItem}>Menu</li>
+
+                <Image src="/Images/logo.png" alt="" width="160px" height="69px" />
+
+
+                <li className={styles.listItem}>Events</li>
+
+                <li className={styles.listItem}>Blog</li>
+
+                <li className={styles.listItem}>Contact</li>
+            </ul>
+
+        </div>
+
+        <div className={styles.item}>
+            <div className={styles.cart}>
+                <Image src="/Images/cart.png" alt="" width="30px" height="30px" />
+                <div className={styles.counter}>2</div>
+            </div>
+        </div>
+
+    </div>
+)
 
 export default Navbar
